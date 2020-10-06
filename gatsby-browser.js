06164b311a18ce07eb664dd1cb00db998487615a
Amelie Ren <meilou@hotmail.fr>
2020-10-06 16:26:37 +0200

    fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -26,29 +26,3 @@ const Provider = ({ element }) => {
 export const wrapPageElement = ({ element }) => {
 	return <Provider element={element} />
 }
-
-// import React, { useState } from 'react'
-// import { ThemeProvider } from 'styled-components'
-
-// import GlobalStyle from './src/styles/GlobalStyle'
-// import { ThemeButton, lightTheme, darkTheme, useDarkMode } from '@styles'
-
-// const Provider = ({ element }) => {
-// 	const [theme, setTheme] = useState(darkTheme)
-
-// 	const handleTheme = () => {
-// 		theme.name === 'light' ? setTheme(darkTheme) : setTheme(lightTheme)
-// 	}
-
-// 	return (
-// 		<ThemeProvider theme={theme}>
-// 			<GlobalStyle />
-// 			<ThemeButton handleTheme={handleTheme} />
-// 			{element}
-// 		</ThemeProvider>
-// 	)
-// }
-
-// export const wrapPageElement = ({ element }) => {
-// 	return <Provider element={element} />
-// }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
-// You can delete this file if you're not using it
+import GlobalStyle from './src/styles/GlobalStyle'
+import { ThemeButton, lightTheme, darkTheme, useDarkMode } from '@styles'
+
+const Provider = ({ element }) => {
+	const [theme, toggleTheme, componentMounted] = useDarkMode()
+
+	const themeMode = theme === 'light' ? lightTheme : darkTheme
+
+	if (!componentMounted) {
+		return <div />
+	}
+
+	return (
+		<ThemeProvider theme={themeMode}>
+			<GlobalStyle />
+			<ThemeButton theme={theme} toggleTheme={toggleTheme} />
+			<h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+			{element}
+		</ThemeProvider>
+	)
+}
+
+export const wrapPageElement = ({ element }) => {
+	return <Provider element={element} />
+}

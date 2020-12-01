5200a22518d70b28df5814b0e68635cf20a2dbc0
Amélie <ren.amelie@gmail.com>
2020-12-01 11:54:21 +0100

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
+import LogRocket from 'logrocket'
+
 import GlobalStyle from './src/styles/GlobalStyle'
 import { lightTheme, darkTheme } from '@styles'
 import { ThemeButton, useDarkMode } from '@components'
 
 const Provider = ({ element }) => {
+	LogRocket.init('wvuvvd/renamelie')
+
 	const [theme, toggleTheme, componentMounted] = useDarkMode()
 
 	const themeMode = theme === 'light' ? lightTheme : darkTheme
